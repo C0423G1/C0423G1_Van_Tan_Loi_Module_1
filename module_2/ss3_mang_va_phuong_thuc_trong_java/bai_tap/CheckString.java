668727bd415a34bcdb239d38aed1e76a14ffe155
C0423G1-VanTanLoi <vantanloi7585@gmail.com>
2023-06-07 21:46:45 +0700

    OK

--- module_2/ss3_mang_va_phuong_thuc_trong_java/bai_tap/CheckString.java
+++ module_2/ss3_mang_va_phuong_thuc_trong_java/bai_tap/CheckString.java
@@ -6,9 +6,17 @@ public class CheckString {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String a = "";
-        System.out.println("Nhập chuỗi vào đây");
+        System.out.print("Nhập chuỗi vào đây");
         a = sc.nextLine();
-
+        System.out.print("Nhập từ cần kiểm tra vào đây ");
+        char b = sc.nextLine().charAt(0);
+        int count = 0;
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) == b) {
+                count++;
+            }
+        }
+        System.out.println("Ký tự " + b + " được lặp lại " + count);
     }
 
 }
