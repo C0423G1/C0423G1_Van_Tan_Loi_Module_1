984555deb25297892f1d6439bfbbc61b44da2f90
C0423G1-VanTanLoi <vantanloi7585@gmail.com>
2023-07-15 15:00:10 +0700

    OK

--- module_3/case_1/quan_li.sql
+++ module_3/case_1/quan_li.sql
@@ -315,4 +315,12 @@ JOIN hop_dong ON nhan_vien.ma_nhan_vien = hop_dong.ma_nhan_vien
 WHERE YEAR(hop_dong.ngay_lam_hop_dong) IN (2021, 2020)
 GROUP BY nhan_vien.ma_nhan_vien, nhan_vien.ho_ten, trinh_do.ten_trinh_do, bo_phan.ten_bo_phan, nhan_vien.so_dien_thoai, nhan_vien.dia_chi
 HAVING COUNT(hop_dong.ma_nhan_vien) < 4;
-
+-- task 16
+SET SQL_SAFE_UPDATES = 0;
+DELETE FROM nhan_vien
+WHERE nhan_vien.ma_nhan_vien NOT IN (select * from
+    (SELECT nhan_vien.ma_nhan_vien
+    FROM nhan_vien
+    LEFT JOIN hop_dong ON nhan_vien.ma_nhan_vien = hop_dong.ma_nhan_vien
+    WHERE YEAR(hop_dong.ngay_lam_hop_dong) IN (2019,2020,2021)) as temp
+);
